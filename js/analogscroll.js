44ecc4a299a7748390c309ad59ff0d7803732482
jackNEss <jackness1208@qq.com>
2016-01-06 01:49:06 +0800

    [ADD]添加相应文件

---
+++ js/analogscroll.js
@@ -0,0 +1,345 @@
+!function($, window, document, undefined){
+    var
+        options = {
+            // 方向 x|y
+            direction: 'y',
+            // 滚动条区域
+            scrollbar: undefined,
+            //滚动条滑块一次点击的移动距离
+            distance: 20,
+            // 向下/右 滚按钮
+            forward: undefined,
+            // 向上/左 滚按钮
+            back: undefined
+        },
+        fn = {
+            preventDefault: function(e){
+                e = e || window.event; 
+                e.preventDefault && e.preventDefault();
+                e.returnValue = false;
+            },
+            stopBubble: function(e){
+                e = e || window.event; 
+                e.stopPropagation && e.stopPropagation();
+                e.cancelBubble = true;
+            },
+            selection: {
+                enable: function(){
+                    document.onselectstart = null;
+                },
+                disable: function(){
+                    document.onselectstart = function(){
+                        return false;
+                    }
+                    window.getSelection
+                        ? window.getSelection().removeAllRanges()
+                        : document.selection.empty()
+                        ;
+                }
+            },
+            getPosition: function(target){var contentDocument = document; arguments[2]? contentDocument = arguments[2]:""; var _x = target.offsetLeft; var _y = target.offsetTop; if($(target).css("position") == "fixed"){_x += contentDocument.documentElement.scrollLeft||contentDocument.body.scrollLeft; _y += contentDocument.documentElement.scrollTop||contentDocument.body.scrollTop; } while(target = target.offsetParent){_x += target.offsetLeft||0; _y += target.offsetTop||0; } return{left:_x, top:_y } }
+            
+        };
+
+    var analogscroll = function(target, op){
+        return new analogscroll.fn.init(target, op);
+    };
+
+    analogscroll.fn = analogscroll.prototype = {
+        op: {},
+
+        el: {
+            target: undefined,
+            scrollbar: undefined,
+            bar: undefined
+        },
+
+        setting: {
+            limitHeight: 0,
+            scale: 1
+        },
+        attrs: [],
+        resize: function(){
+            var she = this,
+                op = she.op,
+                el = she.el,
+                setting = she.setting,
+                attrs = [];
+
+            if( op.direction == "x" ){
+                attrs = ["width","Width","left","Left"];
+
+            } else {
+                attrs = ["height","Height","top","Top"];
+            };
+
+            var seOffset = el.target["offset" + attrs[1]],
+                seScroll = el.target["scroll" + attrs[1]],
+                sbOffset = el.scrollbar["offset" + attrs[1]];
+
+            //可视区域与区域总长之间的比例
+            setting.scale = 0;
+
+            seOffset >= seScroll
+                ? setting.scale = 1 
+                : setting.scale = seOffset / seScroll
+                ;
+
+
+            //区域与滚动条之间的比例
+            setting.b2eScale = sbOffset / seScroll;
+
+            el.bar.style[attrs[0]] = sbOffset * setting.scale + "px";
+            el.bar.style[attrs[2]] = el.target["scroll" + attrs[3]] * setting.b2eScale + "px";
+        },
+        mapping: function(){
+            var she = this,
+                op = she.op,
+                el = she.el,
+                setting = she.setting,
+                attrs = she.attrs;
+
+
+            var nowPostion = parseFloat(el.bar.style[attrs[2]], 10);
+			el.target["scroll" + attrs[3]] = nowPostion / setting.b2eScale;
+        },
+        back: function(){
+            var she = this,
+                op = she.op,
+                el = she.el,
+                setting = she.setting,
+                attrs = she.attrs;
+
+            var nowPosition = parseFloat(el.bar.style[attrs[2]], 10),
+                myPosition,
+                moveDistance = op.distance;
+
+
+            nowPosition - moveDistance > 0
+                ? myPosition =  nowPosition - moveDistance
+                : myPosition = 0
+                ;
+
+            el.bar.style[attrs[2]] = myPosition + "px";
+            she.mapping();
+        },
+        
+        forward: function(){
+            var she = this,
+                op = she.op,
+                el = she.el,
+                setting = she.setting,
+                attrs = she.attrs;
+
+            var nowPosition = parseFloat(el.bar.style[attrs[2]]),
+                myPosition,
+                moveDistance = op.distance,
+                scrollWidth = el.scrollbar["offset" + attrs[1]] - el.bar["offset" + attrs[1]];
+
+
+            nowPosition + moveDistance < scrollWidth
+                ? myPosition = nowPosition + moveDistance
+                : myPosition = scrollWidth
+                ;
+
+            el.bar.style[attrs[2]] = myPosition + "px";
+            she.mapping();
+        }
+    };
+
+    var 
+        init = analogscroll.fn.init = function(target, op){
+            var 
+                she = this,
+                op = she.op = $.fn.extend(undefined, options, op),
+                setting = she.setting,
+                el = she.el = {
+                    target: $(target)[0],
+                    scrollbar: $(she.op.scrollbar)[0],
+                    bar: $(she.op.scrollbar).children()[0],
+                    back: $(she.op.back),
+                    forward: $(she.op.forward)
+                };
+
+            if(!el.target || !el.scrollbar){
+                return;
+            }
+
+            she.resize();
+
+            //滚动条-mousemove事件
+            var attrs = [],
+                mouseEvt = {
+                    // for scrollbar
+                    down: function(e){
+                        e = e || window.event;
+                        fn.preventDefault(e);
+                        fn.stopBubble(e);
+                        setting.posX = e.clientX - this.offsetLeft;
+                        setting.posY = e.clientY - this.offsetTop;
+
+                        fn.selection.disable();
+
+
+                        $(document).bind('mousemove', mouseEvt.move);
+                        $(document).bind('mouseup', mouseEvt.up);
+                    },
+                    // for window
+                    move: function(e){
+                        e = e || window.event;
+            
+                        fn.preventDefault(e);
+                        var nowPostion = e["client" + attrs[4]] - setting["pos" + attrs[4]],
+                            scale = 0;
+
+                        nowPostion < 0? nowPostion = 0:"";
+
+                        nowPostion > (el.scrollbar["offset" + attrs[1]] - el.bar["offset" + attrs[1]])
+                            ? nowPostion = (el.scrollbar["offset" + attrs[1]] - el.bar["offset" + attrs[1]])
+                            : ""
+                            ;
+
+                        el.bar.style[attrs[2]] = nowPostion + "px";
+                        she.mapping();
+                    },
+                    // for window
+                    up: function(){
+                        fn.selection.enable();
+                        $(document).unbind('mousemove', mouseEvt.move);
+                        $(document).unbind('mouseup', mouseEvt.up);
+                    },
+                    // content
+                    wheel: function(e){
+                        e = window.event;
+                        setting.wheelKey = setTimeout(function(){
+                            var myPosition = parseFloat(el.bar.style[attrs[2]], 10),
+                                limitWidth = el.scrollbar["offset" + attrs[1]] - el.bar["offset" + attrs[1]],
+                                moveDistance = op.distance,
+                                data = e.wheelDelta || -e.detail;
+
+                            data > 0? myPosition -= moveDistance : myPosition += moveDistance;
+                            
+                            if(myPosition < 0){ 
+                                myPosition = 0;
+                            } else if(myPosition > limitWidth){
+                                myPosition = limitWidth;
+                            };
+                            el.bar.style[attrs[2]] = myPosition + "px";
+                            she.mapping();
+                            fn.preventDefault(e);
+                        }, 10);
+                        
+                    }
+                };
+
+            if(op.direction == "x"){
+                attrs = she.attrs = ["width","Width","left","Left","X"];
+            } else {
+                attrs = she.attrs = ["height","Height","top","Top","Y"];
+                if("onmousewheel" in el.target){
+                    $(el.target).bind('mousewheel', mouseEvt.wheel);
+
+                } else {
+                    $(el.target).bind('DOMMouseScroll', mouseEvt.wheel);
+
+                }
+            };
+
+            //滚动条滑块初始化
+            el.bar.style[attrs[2]] = 0;
+
+            $(el.scrollbar).bind('mousedown', function(e){
+                var self = this;
+
+                e = e || window.event;
+                fn.preventDefault(e);
+                fn.stopBubble(e);
+                fn.selection.disable();
+
+                setting.posX = e.clientX - self.offsetLeft;
+                setting.posY = e.clientY - self.offsetTop;
+
+
+                $(document).bind("mouseup", function up(e){
+                    clearTimeout(setting.downKey);
+                    fn.selection.enable();
+                    $(document).unbind('mouseup', up);
+                });
+
+                var distance = op.distance;
+                barWidth = el.bar["offset" + attrs[1]];
+
+                var myPosition;
+                setting.mousePosition = e["client" + attrs[4]] + (document.documentElement["scroll" + attrs[3]] || document.body["scroll" + attrs[3]]) - fn.getPosition(this)[attrs[2]];
+
+
+                !function move(){
+                    clearTimeout(setting.downKey);
+
+                    var barPosition = parseFloat(el.bar.style[attrs[2]], 10),
+                        mousePosition = setting.mousePosition;
+
+                    if(mousePosition < barPosition){
+                        she.back();
+
+                    } else if( mousePosition > barPosition + barWidth){
+                        she.forward();
+
+                    } else {
+                        return;
+
+                    };
+
+                    setting.downKey = setTimeout( move, 50);
+                }();
+            });
+
+            $(el.bar).bind('mousedown', mouseEvt.down);
+
+            $(el.forward).bind('mousedown', function(e){
+                e = e || window.event;
+                clearTimeout(setting.downKey);
+                fn.preventDefault(e);
+
+                $(document).bind("mouseup", function up(){
+                    clearTimeout(setting.downKey);
+                    $(document).unbind('mouseup',up);
+                });
+
+                !function move(){
+                    she.forward();
+                    setting.downKey = setTimeout(move,50);
+                }();
+            });
+
+            $(el.back).bind('mousedown', function(e){
+                e = e || window.event;
+                clearTimeout(setting.downKey);
+                fn.preventDefault(e);
+
+                $(document).bind("mouseup", function up(){
+                    clearTimeout(setting.downKey);
+                    $(document).unbind('mouseup',up);
+                });
+
+                !function move(){
+                    she.back();
+                    setting.downKey = setTimeout(move,50);
+                }();
+            });
+
+            return this;
+        };
+
+    init.prototype = analogscroll.fn;
+
+
+    if(typeof define != 'undefined' && defined.amd){
+        define([], function(){
+            return analogscroll;
+        });
+
+    } else {
+        window.analogscroll = analogscroll;
+    }
+}(jQuery, window, document);
---
+++ readme.md
@@ -0,0 +1,60 @@
+# analogscroll 模拟滚动 组件功能说明
+
+## 组件对 UI 的要求：
+* options.scrollbar 为滚动区域对象, 组件要求滚动区域内有且只有 1个 子级元素， 组件会将该子级元素认定为滚动条区域中的滑块
+```
+<div class="scroll-area">
+    <span class="bar"></span>
+</div>
+```
+* target 为需要添加滚动条的主体内容, 需通过样式设置成 overflow: hidden， 组件才能正常渲染。
+```
+<div class="main-area" style="overflow: hidden;">
+    <div class="text-cnt">
+    some text...
+    </div>
+</div>
+```
+
+## 使用方法
+```
+var iScroll = analogscroll(
+    '#list',
+    {
+        direction: 'x',
+        distance: 20,
+        scrollbar: '#vScrollbar',
+        forward: '#vBottomBtn',
+        back: '#vTopBtn'
+    }
+);
+```
+## 参数说明
+```
+analogscroll(target, options);
+```
+|参数|类型|是否必须|说明|
+|----|----|--------|----|
+|target|{string/object}|是|需要添加滚动条的主体内容|
+|options|{object}|是|参数设置|
+
+### options 参数属性说明
+
+|参数|类型|是否必须|说明|
+|----|----|--------|----|
+|scrollbar|{string/object}|是|滚动区域对象|
+|direction|{string}|否|滚动条方向。x 或者 y。默认值为 y|
+|distance|{number}|否|鼠标滚轮每滚一下滑移动的距离。默认值为 20|
+|forward|{string/object}|否|控制滚动条向右/下 移动的按钮|
+|back|{string/object}|否|控制滚动条向左/上 移动的按钮|
+
+## 返回对象
+```
+var iScroll = analogscroll(target, options);
+```
+### iScroll 参数说明
+|参数|类型|说明|
+|----|----|----|
+|resize|{function}|滚动条区域大小重新计算|
+|back|{function}|控制滚动条向左/上移动|
+|forward|{function}|控制滚动条向右/下移动|
